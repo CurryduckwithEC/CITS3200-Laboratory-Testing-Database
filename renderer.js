515d032475b283ae46f1631af5b42c05cb81807f
ST <166231158+st252@users.noreply.github.com>
2024-08-29 03:09:45 +0800

    added renderer for javascript functions

---
+++ renderer.js
@@ -0,0 +1,66 @@
+function checkFilters() {
+  console.log("In checking filters");
+  const form = document.getElementById("filterForm");
+  const output = document.getElementById("filterOutput");
+
+  // Clear any previous output
+  output.innerHTML = "Testing Button Click<br>";
+
+  // Drainage 
+  let drainage = getCheckedValues("drainage[]");
+  if (drainage.length > 0) {
+    output.innerHTML += "<b>Drainage:<b> " + drainage.join(', ') + "<br>";
+  }
+ 
+  // Shearing
+  let shearing = getCheckedValues("shearing[]");
+  if (shearing.length > 0) {
+    output.innerHTML += "<b>Shearing:<b> " + shearing.join(', ') + "<br>";
+  }
+
+  // Anisotropy
+  let anisotropy = getCheckedValues("anisotropy[]");
+  if (anisotropy.length > 0) {
+    output.innerHTML += "<b>Anisotropy:<b> " + anisotropy.join(', ') + "<br>";
+  }
+  const minAnisotropyInput = document.getElementById("filter-aniso-min");
+  const maxAnisotropyInput = document.getElementById("filter-aniso-max");
+  const minAnisotropyValue = minAnisotropyInput.value;
+  const maxAnisotropyValue = maxAnisotropyInput.value;
+  output.innerHTML += `<b>Min Anisotropy:</b> ${minAnisotropyValue}<br>`;
+  output.innerHTML += `<b>Max Anisotropy:</b> ${maxAnisotropyValue}<br>`;
+
+
+  // Consolidation
+  const minConsolidationInput = document.getElementById("filter-con-min");
+  const maxConsolidationInput = document.getElementById("filter-con-max");
+  const minConsolidationValue = minConsolidationInput.value;
+  const maxConsolidationValue = maxConsolidationInput.value;
+  output.innerHTML += `<b>Min Consolidation:</b> ${minConsolidationValue}<br>`;
+  output.innerHTML += `<b>Max Consolidation:</b> ${maxConsolidationValue}<br>`; 
+
+
+
+}
+
+function getCheckedValues(name) {
+  const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+  return Array.from(checkboxes).map(checkbox => checkbox.value);
+}
+
+function getConsolidationValues(name) {
+
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('check-btn');
+  if (button) {
+    button.addEventListener('click', checkFilters);
+
+    
+
+  } else {
+    console.error('Button with id "check-btn" not found');
+  }
+});
