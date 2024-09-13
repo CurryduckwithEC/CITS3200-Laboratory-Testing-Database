1086d3dcc6ad2abe2174973c89e88cac7359267e
DominiAetheres <ethandai888@gmail.com>
2024-09-13 11:48:15 +0800

    Added file name to database commit function

--- api/datahandler.py
+++ api/datahandler.py
@@ -52,7 +52,7 @@ def test_values_object(specs: dict) -> TestValues:
 
     return test_values
 
-def test_object(specs: dict, sample_values: SampleValues, test_values: TestValues, file_path: str) -> Test:
+def test_object(specs: dict, sample_values: SampleValues, test_values: TestValues, file_name: str) -> Test:
 
     # Make sure to commit any foreign key dependencies first
     test = Test(
@@ -62,7 +62,7 @@ def test_object(specs: dict, sample_values: SampleValues, test_values: TestValue
         consolidation = specs["consolidation"],
         anisotropy = specs["anisotropy"],
 
-        test_file_name = file_path
+        test_file_name = file_name
     )
 
     return test
@@ -90,11 +90,11 @@ def entry_objects(df: pd.DataFrame, test: Test):
 # Function that takes the data of a new entry and commits to database
 # The ordering matters of the commit due to foreign key dependencies
 # Takes absolute path to database
-def commit_new_entry(specs: dict, df: pd.DataFrame):
+def commit_new_entry(specs: dict, df: pd.DataFrame, file_name: str):
 
     sample_values = sample_values_object(specs)
     test_values = test_values_object(specs)
-    test = test_object(specs, sample_values, test_values)
+    test = test_object(specs, sample_values, test_values, file_name)
 
     entries = entry_objects(df, test)
 
